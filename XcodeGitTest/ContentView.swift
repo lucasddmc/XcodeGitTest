4ed40298e06e8c625039136e796ffb29b2fa5be8
Lucas Carvalho <lucasddmc@outlook.com>
2024-08-26 16:25:34 -0300

    commitzinho interrogação

--- XcodeGitTest/ContentView.swift
+++ XcodeGitTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Lucas!")
+            Text("Hello, Lucas?")
         }
         .padding()
     }
