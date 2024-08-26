9ea43015f9a4799c0a3aa3370520ddaa485996be
Lucas Carvalho <lucasddmc@outlook.com>
2024-08-26 15:54:55 -0300

    Initial Commit

---
+++ XcodeGitTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeGitTest
+//
+//  Created by Lucas Dantas de Moura Carvalho on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeGitTest/XcodeGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitTestApp.swift
+//  XcodeGitTest
+//
+//  Created by Lucas Dantas de Moura Carvalho on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
